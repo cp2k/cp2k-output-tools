b659b4df6e0c690a008602831cebd220f85a9154
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-02-18 17:45:06 +0100

    kv_sections: convert bool and int

--- cp2k_output_tools/blocks/kv_sections.py
+++ cp2k_output_tools/blocks/kv_sections.py
@@ -19,7 +19,20 @@ def match_kv_sections(content):
         if section not in result:
             result[section] = {}
 
-        result[section][match["key"].lower()] = match["value"]
+        key = match["key"].lower()
+        value = match["value"]
+
+        if value in ["T", "F"]:
+            result[section][key] = True if value == "T" else False
+            continue
+
+        try:
+            result[section][key] = int(value)
+            continue
+        except ValueError:
+            pass
+
+        result[section][key] = value
 
     if not result:
         return None
