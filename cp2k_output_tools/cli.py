65e696042e2a96dfe143615267433533ff25af4b
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-02-19 15:47:31 +0100

    CLI: add possibility for yaml output

--- cp2k_output_tools/cli.py
+++ cp2k_output_tools/cli.py
@@ -15,6 +15,7 @@ def cp2kparse():
         default=sys.stdin,
         help="CP2K output file, stdin if not specified",
     )
+    parser.add_argument("-y", "--yaml", action="store_true", help="output yaml instead of json")
     args = parser.parse_args()
 
     tree = {}
@@ -22,4 +23,11 @@ def cp2kparse():
     for match in parse_iter(args.file.read()):
         tree.update(match)
 
-    print(json.dumps(tree, indent=2, sort_keys=True))
+    if args.yaml:
+        from ruamel.yaml import YAML
+
+        yaml = YAML()
+
+        yaml.dump(tree, sys.stdout)
+    else:
+        print(json.dumps(tree, indent=2, sort_keys=True))
