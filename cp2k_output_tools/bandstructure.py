66862f3e639539beb15047dcb80aa835f8b1cf19
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2020-05-05 18:17:56 +0200

    bs2csv: add parser for CP2K v8+ format

--- cp2k_output_tools/bandstructure.py
+++ cp2k_output_tools/bandstructure.py
@@ -2,10 +2,33 @@
 Convert the CP2K band structure output to CSV files
 """
 
+__all__ = ["SpecialPoint", "Point", "set_gen"]
+
 import re
 import argparse
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
+import itertools
+
+
+@dataclass
+class SpecialPoint:
+    number: int
+    name: str
+    a: float
+    b: float
+    c: float
+
+
+@dataclass
+class Point:
+    a: float
+    b: float
+    c: float
+    bands: List[float]
+    spin: int
+    weight: Optional[float] = None
+
 
 SET_MATCH = re.compile(
     r"""
@@ -36,7 +59,7 @@ POINTS_MATCH = re.compile(
   K-Point [ ]+ (?P<a>\S+) [ ]+ (?P<b>\S+) [ ]+ (?P<c>\S+) [ ]*
   \n
 [ ]* (?P<npoints>\d+) [ ]* \n
-(?P<values>
+(?P<bands>
   [\s\S]*?(?=\n.*?[ ] Nr|$)  # match everything until next 'Nr.' or EOL
 )
 """,
@@ -44,41 +67,94 @@ POINTS_MATCH = re.compile(
 )
 
 
-@dataclass
-class SpecialPoint:
-    number: int
-    name: str
-    a: float
-    b: float
-    c: float
-
-
-@dataclass
-class Point:
-    a: float
-    b: float
-    c: float
-    values: List[float]
-
-
-def specialpoints_gen(content):
+def _specialpoints_gen(content):
     for match in SPOINTS_MATCH.finditer(content):
         yield SpecialPoint(int(match["number"]), match["name"], float(match["a"]), float(match["b"]), float(match["c"]))
 
 
-def points_gen(content):
+def _points_gen(content):
     for match in POINTS_MATCH.finditer(content):
-        yield Point(float(match["a"]), float(match["b"]), float(match["c"]), [float(v) for v in match["values"].split()])
+        yield Point(
+            a=float(match["a"]),
+            b=float(match["b"]),
+            c=float(match["c"]),
+            bands=[float(v) for v in match["bands"].split()],
+            spin=int(match["spin"]),
+        )
+
+
+SET_MATCH8 = re.compile(
+    r"""
+\#\ Set\ (?P<setnr>\d+):\ \d+\ special\ points,\ (?P<totalpoints>\d+)\ k-points,\ \d+\ bands \s*
+(?P<content>
+  [\s\S]*?(?=\n.*?\#\ Set|$)  # match everything until next 'SET' or EOL
+)
+""",
+    re.VERBOSE,
+)
+
+
+SPOINTS_MATCH8 = re.compile(
+    r"""
+\#\s+ Special\ point\ (?P<number>\d+) \s+ (?P<a>\S+) \s+ (?P<b>\S+) \s+ (?P<c>\S+) \s+ (?P<name>\S+)
+""",
+    re.VERBOSE,
+)
+
+
+POINTS_MATCH8 = re.compile(
+    r"""
+\#\ \ Point\ (?P<nr>\d+)\s+ Spin\ (?P<spin>\d+): \s+ (?P<a>\S+) \s+ (?P<b>\S+) \s+ (?P<c>\S+) [ ]* ((?P<weight>\S+) [ ]*)? \n
+\#\ \ \ Band \s+ Energy\ \[eV\] \s+ Occupation \s*
+(?P<bands>
+  [\s\S]*?(?=\n.*?\#\ \ Point|$)  # match everything until next '# Point' or EOL
+)
+""",
+    re.VERBOSE,
+)
+
+
+def _points_gen8(content):
+    for match in POINTS_MATCH8.finditer(content):
+        try:
+            weight = float(match["weight"])
+        except TypeError:
+            weight = None
+
+        values = match["bands"].split()
+
+        yield Point(
+            a=float(match["a"]),
+            b=float(match["b"]),
+            c=float(match["c"]),
+            bands=[float(v) for v in values[1::3]],
+            weight=weight,
+            spin=int(match["spin"]),
+        )
+
+
+def _specialpoints_gen8(content):
+    for match in SPOINTS_MATCH8.finditer(content):
+        yield SpecialPoint(int(match["number"]), match["name"], float(match["a"]), float(match["b"]), float(match["c"]))
 
 
 def set_gen(content):
-    for match in SET_MATCH.finditer(content):
-        yield (
-            int(match["setnr"]),
-            int(match["totalpoints"]),
-            iter(specialpoints_gen(match["content"])),
-            iter(points_gen(match["content"])),
-        )
+    # try with the CP2K+8+ regex first
+    matchiter = SET_MATCH8.finditer(content)
+    specialpoints_gen = _specialpoints_gen8
+    points_gen = _points_gen8
+
+    try:
+        peek = next(matchiter)
+        matchiter = itertools.chain([peek], matchiter)
+    except StopIteration:
+        # if nothing could be found, fallback to the older format
+        matchiter = SET_MATCH.finditer(content)
+        specialpoints_gen = _specialpoints_gen
+        points_gen = _points_gen
+
+    for match in matchiter:
+        yield (int(match["setnr"]), int(match["totalpoints"]), specialpoints_gen(match["content"]), points_gen(match["content"]))
 
 
 def cp2k_bs2csv():
@@ -97,7 +173,9 @@ def cp2k_bs2csv():
     )
     args = parser.parse_args()
 
-    for setnr, totalpoints, specialpoints, points in set_gen(args.bsfile.read()):
+    content = args.bsfile.read()
+
+    for setnr, totalpoints, specialpoints, points in set_gen(content):
         filename = args.output_pattern.format(bsfile=args.bsfile, setnr=setnr)
 
         with open(filename, "w") as csvout:
@@ -105,10 +183,10 @@ def cp2k_bs2csv():
             print("with the following special points:")
 
             for point in specialpoints:
-                print(f"  {point.name:>8}: {point.a:10.8f}/{point.b:10.8f}/{point.c:10.8f}")
+                print(f"  {point.name:>8}: {point.a:10.8f} / {point.b:10.8f} / {point.c:10.8f}")
 
             for point in points:
                 csvout.write(f"{point.a:10.8f} {point.b:10.8f} {point.c:10.8f}")
-                for value in point.values:
+                for value in point.bands:
                     csvout.write(f" {value:10.8f}")
                 csvout.write("\n")
--- tests/test_bs2csv.py
+++ tests/test_bs2csv.py
@@ -4,9 +4,18 @@ from . import TEST_DIR
 
 
 @pytest.mark.script_launch_mode("subprocess")
-def test_bs2csv(script_runner):
+def test_bs2csv_cp2k7_input(script_runner):
     ret = script_runner.run("cp2k_bs2csv", str(TEST_DIR / "inputs" / "WO3.cp2k-7.bs"))
 
     assert ret.success
     assert "total number of k-points: 11" in ret.stdout
-    assert "GAMMA: 0.00000000/0.00000000/0.00000000" in ret.stdout
+    assert "GAMMA: 0.00000000 / 0.00000000 / 0.00000000" in ret.stdout
+
+
+@pytest.mark.script_launch_mode("subprocess")
+def test_bs2csv_cp2k8_input(script_runner):
+    ret = script_runner.run("cp2k_bs2csv", str(TEST_DIR / "inputs" / "WO3.cp2k-8.bs"))
+
+    assert ret.success
+    assert "total number of k-points: 11" in ret.stdout
+    assert "GAMMA: 0.00000000 / 0.00000000 / 0.00000000" in ret.stdout
